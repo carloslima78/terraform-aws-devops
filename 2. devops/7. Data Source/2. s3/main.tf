ba87e446751c5e706600ca3b8fa13c1c5d2308ea
carloslima78 <carlos.lima78@gmail.com>
2023-04-03 21:15:24 -0300

    inclusão do data source

--- 2. devops/7. Data Source/2. s3/main.tf
+++ 2. devops/7. Data Source/2. s3/main.tf
@@ -1,23 +1,19 @@
-
 terraform {
 
   required_version = ">=1.3.7"
 
-  # Loca o Provider utilizado
   required_providers {
 
     aws = {
 
-      # Dados da origem e versão, encontrados no arquivo ".terraform.lock.hcl"
       source  = "hashicorp/aws"
       version = "4.58.0"
 
     }
   }
+}
 
-  provider "aws" {
+provider "aws" {
 
-    region = var.aws-region
-  }
-
+  region = var.aws-region
 }
